83e38de50ab1f640c51bec3b7384296d6c5a45be
Phát Trần <phat.tranducvn@gmail.com>
2022-03-29 09:56:19 -0700

    Add hom fragment

--- app/src/main/java/com/example/phat_coding_assignment_3/MainActivity.kt
+++ app/src/main/java/com/example/phat_coding_assignment_3/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.phat_coding_assignment_3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.phat_coding_assignment_3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // Setup view binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Retrieve NavController from the NavHostFragment
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        // Set up the action bar for use with the NavController
+        setupActionBarWithNavController(navController)
+    }
+
+
+    /**
+     * Handle navigation when the user chooses Up from the action bar.
+     */
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
